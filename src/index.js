a5f08e602ba295b99e05b1b6f3707a5ba788cea3
Saurabh Karn <saurabhkarn007@gmail.com>
2023-06-04 17:58:34 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
-const express= require('express')
-const bodyParser=require('body-parser')
-const route=require('./routes/route.js')
-const mongoose=require('mongoose')
-const multer=require('multer')
-const app=express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const route = require("./routes/route.js");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const app = express();
 
-mongoose.set('strictQuery',true)
+mongoose.set("strictQuery", true);
 
-app.use(bodyParser.json())
-app.use(multer().any())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json());
+app.use(multer().any());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://vishalsharma:8423354673@vishal-db.bpwswlx.mongodb.net/group21Database",{
-    useNewUrlParser:true
-})
-.then(()=> console.log("MongoDB is connected..."))
-.catch(err=>console.log(err))
+mongoose
+  .connect(
+    "mongodb+srv://vishalsharma:8423354673@vishal-db.bpwswlx.mongodb.net/group21Database",
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => console.log("MongoDB is connected..."))
+  .catch((err) => console.log(err));
 
-app.use('/',route)
+app.use("/", route);
 
-app.listen(process.env.PORT || 3001, function(){
-    console.log('express app running on port' + " " + (process.env.PORT || 3001))
-})
+app.listen(process.env.PORT || 3001, function () {
+  console.log("express app running on port" + " " + (process.env.PORT || 3001));
+});
